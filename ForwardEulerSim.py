376bcdef296b7d19fc5173cdb068e36a7133b00b
TaiskeColclasure <taco6155@colorado.edu>
2023-01-31 17:33:05 -0700

    Forward Euler complete

--- ForwardEulerSim.py
+++ ForwardEulerSim.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-import numpy as  np
+import numpy as np
 
 #initialize global variables
 N = 1000
@@ -7,23 +7,45 @@ I = 1
 S = N - I
 R=0
 t = 0
-timeCap = 1
+timeCap = 50
 gamma = .5
-betta = 1.5
+betta = 1
+deltaT = .5
 
 #Conntainers
 sBucket = [S]
 iBucket = [I]
 rBucket = [R]
+tBucket = [0]
 t = 0
 
-def calcForwardStep(_t):
-    incidents = (betta * sBucket * I) / N
-    recoveries = gamma * I
+def displayResults():
+    plt.plot(tBucket, sBucket, label = "S taiske")
+    plt.plot(tBucket, iBucket, label = "I taiske")
+    plt.plot(tBucket, rBucket, label = "R taiske")
+    plt.legend()
+    plt.title('SIR model with beta: {}  and gamma: {}'.format(betta, gamma))
+    plt.savefig('SIR_model_beta-{}_gamma:_{}.jpg'.format(betta, gamma))
+
+def calcForwardStep():
+    incidents = (betta * sBucket[-1] * iBucket[-1]) / N
+    recoveries = gamma * iBucket[-1] 
     print('Incidents: {} \nRecoveries: {}'.format(incidents, recoveries))
-    return _S, _I, _R
+    sSot = -incidents
+    iDot = incidents - recoveries
+    rDot = recoveries
+    return -incidents, incidents - recoveries, recoveries
 
+def applyStepChanges(_sDot, _iDot, _rDot):
+    sBucket.append(sBucket[-1] + (deltaT * _sDot))
+    iBucket.append(iBucket[-1] + (deltaT * _iDot))
+    rBucket.append(rBucket[-1] + (deltaT * _rDot))
+    tBucket.append(tBucket[-1] + deltaT)
+    
+#Time loop
 while t < timeCap:
-    sDot, iDot, rDot = calcForwardStep(t)
-    i+=1
+    sDot, iDot, rDot = calcForwardStep()
+    applyStepChanges(sDot, iDot, rDot)
+    t += deltaT
 
+displayResults()
